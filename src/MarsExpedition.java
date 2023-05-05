03866cd2b372e89e03e20e5712ac39254881fb9f
Julian Baranowski <julian.paul.baranowski@gmail.com>
2023-05-04 22:48:52 -0600

    third commit for MarsExpedition

--- src/MarsExpedition.java
+++ src/MarsExpedition.java
@@ -21,11 +21,25 @@ public class MarsExpedition {
         System.out.println("Hello " + name + " ----- Welcome to the Expedition prep program.\n" +
                 "Are you ready to head out into the new world?\n" +
                 "Type Y or N");
+
         String init = input.nextLine();
+
         if (init.equalsIgnoreCase("Y")) {
             System.out.println("I knew you would say that! You are a team leader for a reason!");
         } else {
             System.out.println("Too bad you are the team leader. You have to go!");
+        }
+        System.out.println("How many people do you require for your expedition team? (Int input required)");
+
+        int teamSize = input.nextInt();
+        input.nextLine();
+
+        if (teamSize > 2) {
+            System.out.println("That's way to many people. We can only send 2 more members.");
+        } else if (teamSize < 2) {
+            System.out.println("That's not enough people. We need you and 2 more members.");
+        } else if ( teamSize == 2) {
+            System.out.println("That's a perfect sized team. Good job!");
         }
     }
 }
