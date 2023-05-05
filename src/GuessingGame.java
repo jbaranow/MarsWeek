f5122f90ee1ab7efaf8eda7e4b05aafe618fcc67
Julian Baranowski <julian.paul.baranowski@gmail.com>
2023-05-04 21:45:38 -0600

    second commit

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class GuessingGame {
     public  static void main(String[] args) {
@@ -8,5 +10,26 @@ public class GuessingGame {
         Scanner input = new Scanner(System.in);
 
         String name = input.nextLine();
+
+        System.out.println(name + ", I'm thinking of a number between 1 and 100.");
+        System.out.println("Can you guess what the number is?");
+
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        int tries = 0;
+
+        while (true) {
+            int guess = input.nextInt();
+
+            tries = tries + 1;
+
+            if (guess < number) {
+                System.out.println("Your guess is too low, please try again!");
+            } else if (guess > number) {
+                System.out.println("Your guess is too high, please try again!");
+            }
+        }
+
+        //Scanner
     }
 }
